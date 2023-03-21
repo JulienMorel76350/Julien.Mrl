30554dac08dd4c8d973ff6372af864243c83e91c
JulienMorel76350 <Julien.morel76350@gmail.com>
2023-03-21 22:29:35 +0100

    🚨 Fix for build

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,55 +1,77 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Navbar } from '@/components/Navbar';
-import { ProjectRow } from '@/components/ProjectRow';
+import { Navbar } from '@/components/Navbar'
+import { ProjectRow } from '@/components/ProjectRow'
 const Title = styled.h1`
-    font-weight: 700;
-    font-size: 60px;
-    color: #FFFFFF;
-    text-align:center;
+  font-weight: 700;
+  font-size: 60px;
+  color: #ffffff;
+  text-align: center;
 `
 const Wrap = styled.div`
-    display:flex;
-    margin:20px;
-    gap:20px;
+  display: flex;
+  margin: 20px;
+  gap: 20px;
 `
 const Content = styled.div`
-    width:60%;
-    min-width:fit-content;
+  width: 60%;
+  min-width: fit-content;
 `
 const Bar = styled.div`
-    border: 1px solid #FFFFFF;
-    width:100%;
+  border: 1px solid #ffffff;
+  width: 100%;
 `
 const ImgContainer = styled.div`
-    display:flex;
-    width:40%;
-    img {
-        width:100%;
-        height:100%;
-    }
+  display: flex;
+  width: 40%;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `
 export default function Project() {
-    const [productType, setProductType] = useState<undefined | string>('');
-    return (
-        <>
-            <Navbar />
-            <Title>
-                Mes Projets
-            </Title>
-            <Wrap>
-                <Content>
-                    <Bar></Bar>
-                    <ProjectRow status={true} socity="NEED FOR SCHOOL" title="League of legends" skills='Symfony API Front React axios' productTypeValue={productType}  onProductTypeChange={setProductType} />
-                    <ProjectRow status={true} socity="NEED FOR SCHOOL" title="Creation of a CRM" skills='Symfony API Front Vue 2' />
-                    <ProjectRow status={false} socity="MES PARENTS & MOI" title="WebSite M&M" skills='Wordpress' />
-                    <ProjectRow status={false} socity="COURSES & MOI" title="WebSite C&M" skills='Wordpress' />
-                    <ProjectRow status={true} socity="COURSES & MOI" title="WebSite C&M" skills='Wordpress' />
-                </Content>
-                <ImgContainer>
-                    <img src="3fontainebleau.jpg" alt="" />
-                </ImgContainer>
-            </Wrap>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Title>Mes Projets</Title>
+      <Wrap>
+        <Content>
+          <Bar></Bar>
+          <ProjectRow
+            status={true}
+            socity="NEED FOR SCHOOL"
+            title="League of legends"
+            skills="Symfony API Front React axios"
+          />
+          <ProjectRow
+            status={true}
+            socity="NEED FOR SCHOOL"
+            title="Creation of a CRM"
+            skills="Symfony API Front Vue 2"
+          />
+          <ProjectRow
+            status={false}
+            socity="MES PARENTS & MOI"
+            title="WebSite M&M"
+            skills="Wordpress"
+          />
+          <ProjectRow
+            status={false}
+            socity="COURSES & MOI"
+            title="WebSite C&M"
+            skills="Wordpress"
+          />
+          <ProjectRow
+            status={true}
+            socity="COURSES & MOI"
+            title="WebSite C&M"
+            skills="Wordpress"
+          />
+        </Content>
+        <ImgContainer>
+          <img src="3fontainebleau.jpg" alt="" />
+        </ImgContainer>
+      </Wrap>
+    </>
+  )
 }
